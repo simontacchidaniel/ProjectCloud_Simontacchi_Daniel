4a2ecc45f4a9cfc9ba55d0da0df66eab98597338
simontacchidaniel <simontacchidaniel@gmail.com>
2026-04-01 18:12:47 +0200

    Update reader.py

--- ProgettoCloud/reader/reader.py
+++ ProgettoCloud/reader/reader.py
@@ -25,4 +25,4 @@ def get_data():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
